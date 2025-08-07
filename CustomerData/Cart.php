d4669f1d307933b83efd78810d0bcbf85a969c12
Yehor Shytikov <egorshitikov@gmail.com>
2025-08-07 07:40:48 +0000

    add checkout button

--- CustomerData/Cart.php
+++ CustomerData/Cart.php
@@ -42,70 +42,76 @@ class Cart extends MagentoCart implements SectionSourceInterface
      */
     public function renderCartHtml($data)
     {
-        $html = '<div class="block-title">';
-        $html .= '<strong>';
-        $html .= '<span class="text">My Cart</span>';
-        $html .= '<span class="qty" title="Items in Cart">' . $data['summary_count'] . '</span>';
-        $html .= '</strong>';
-        $html .= '</div>';
-
-        $html .= '<div class="block-content">';
-        $html .= '<button type="button" id="btn-minicart-close" class="action close" data-action="close" title="Close">';
-        $html .= '<span>Close</span>';
-        $html .= '</button>';
-
+        // Build items HTML
+        $itemsHtml = '';
         if (!empty($data['items'])) {
             // Debug: Show available item data
-            $html .= '<!-- Debug: Item data structure -->';
-            $html .= '<!-- ' . json_encode($data['items'][0]) . ' -->';
+            $debugComment = '<!-- Debug: Item data structure -->' . PHP_EOL . '<!-- ' . json_encode($data['items'][0]) . ' -->';
             
-            $html .= '<div class="items-total">';
-            $html .= '<span class="count">' . $data['summary_count'] . '</span>';
+            $itemsHtml = $debugComment . '
+            <div class="items-total">
+                <span class="count">' . $data['summary_count'] . '</span>';
             if ($data['summary_count'] > 1) {
-                $html .= '<span>Items in Cart</span>';
+                $itemsHtml .= '<span>Items in Cart</span>';
             } else {
-                $html .= '<span>Item in Cart</span>';
+                $itemsHtml .= '<span>Item in Cart</span>';
             }
-            $html .= '</div>';
+            $itemsHtml .= '</div>
 
-            $html .= '<div class="subtotal">';
-            $html .= '<span class="label">Cart Subtotal</span>';
-            $html .= '<div class="amount price-container">';
-            $html .= '<span class="price-wrapper">';
-            $html .= '<span class="price">' . $data['subtotal'] . '</span>';
-            $html .= '</span>';
-            $html .= '</div>';
-            $html .= '</div>';
+            <div class="subtotal">
+                <span class="label">Cart Subtotal</span>
+                <div class="amount price-container">
+                    <span class="price-wrapper">
+                        <span class="price">' . $data['subtotal'] . '</span>
+                    </span>
+                </div>
+            </div>
 
-            $html .= '<div class="actions">';
-            $html .= '<div class="primary">';
-            $html .= '<button id="top-cart-btn-checkout" type="button" class="action primary checkout" data-action="close" title="Proceed to Checkout">Proceed to Checkout</button>';
-            $html .= '</div>';
-            $html .= '</div>';
+            <div class="actions">
+                <div class="primary">
+                    <button id="top-cart-btn-checkout" type="button" class="action primary checkout" data-action="close" onclick="window.location.href=\'/checkout/\'" title="Proceed to Checkout">Proceed to Checkout</button>
+                </div>
+            </div>
 
-            $html .= '<strong class="subtitle">Recently added item(s)</strong>';
-            $html .= '<div data-action="scroll" class="minicart-items-wrapper">';
-            $html .= '<ol id="mini-cart" class="minicart-items">';
+            <strong class="subtitle">Recently added item(s)</strong>
+            <div data-action="scroll" class="minicart-items-wrapper">
+                <ol id="mini-cart" class="minicart-items">';
+            
             foreach ($data['items'] as $item) {
-                $html .= $this->renderCartItem($item);
+                $itemsHtml .= $this->renderCartItem($item);
             }
-            $html .= '</ol>';
-            $html .= '</div>';
+            
+            $itemsHtml .= '</ol>
+            </div>
 
-            $html .= '<div class="actions">';
-            $html .= '<div class="secondary">';
-            $html .= '<a class="action viewcart" href="/checkout/cart/">';
-            $html .= '<span>View and Edit Cart</span>';
-            $html .= '</a>';
-            $html .= '</div>';
-            $html .= '</div>';
+            <div class="actions">
+                <div class="secondary">
+                    <a class="action viewcart" href="/checkout/cart/">
+                        <span>View and Edit Cart</span>
+                    </a>
+                </div>
+            </div>';
         } else {
-            $html .= '<div class="empty-cart">';
-            $html .= '<p>Your cart is empty</p>';
-            $html .= '</div>';
+            $itemsHtml = '<div class="empty-cart">
+                <p>Your cart is empty</p>
+            </div>';
         }
 
-        $html .= '</div>';
+        // Build complete cart HTML using heredoc
+        $html = <<<HTML
+        <div class="block-title">
+            <strong>
+                <span class="text">My Cart</span>
+                <span class="qty" title="Items in Cart">{$data['summary_count']}</span>
+            </strong>
+        </div>
+        <div class="block-content">
+            <button type="button" id="btn-minicart-close" class="action close" data-action="close" title="Close">
+                <span>Close</span>
+            </button>
+            {$itemsHtml}
+        </div>
+HTML;
         
         return $html;
     }
@@ -118,99 +124,93 @@ class Cart extends MagentoCart implements SectionSourceInterface
      */
     protected function renderCartItem($item)
     {
-        $html = '<li class="item product product-item" data-role="product-item">';
-        $html .= '<div class="product">';
-        
-        // Product image
-        if (isset($item['product_url'])) {
-            $html .= '<a href="' . $item['product_url'] . '" class="product-item-photo" title="' . $item['product_name'] . '">';
-            
-            // Try different possible image sources
-            $imageSrc = '';
-            if (isset($item['product_image']['src'])) {
-                $imageSrc = $item['product_image']['src'];
-            } elseif (isset($item['product_image'])) {
-                $imageSrc = $item['product_image'];
-            } elseif (isset($item['thumbnail'])) {
-                $imageSrc = $item['thumbnail'];
-            }
-            
-            if ($imageSrc) {
-                $html .= '<span class="cart-product-image-container">';
-                $html .= '<span class="cart-product-image-wrapper">';
-                $html .= '<img class="cart-product-image-photo" src="' . $imageSrc . '" alt="' . $item['product_name'] . '" style="width: 120px;">';
-                $html .= '</span>';
-                $html .= '</span>';
-            } else {
-                // Fallback placeholder
-                $html .= '<span class="cart-product-image-container">';
-                $html .= '<span class="cart-product-image-wrapper">';
-                $html .= '<img class="cart-product-image-photo" src="/media/catalog/product/placeholder/default/image.jpg" alt="' . $item['product_name'] . '" style="width: 120px;">';
-                $html .= '</span>';
-                $html .= '</span>';
-            }
-            $html .= '</a>';
+        // Get image source with fallbacks
+        $imageSrc = '';
+        if (isset($item['product_image']['src'])) {
+            $imageSrc = $item['product_image']['src'];
+        } elseif (isset($item['product_image'])) {
+            $imageSrc = $item['product_image'];
+        } elseif (isset($item['thumbnail'])) {
+            $imageSrc = $item['thumbnail'];
         }
         
-        $html .= '<div class="product-item-details">';
-        
-        // Product name
-        $html .= '<strong class="product-item-name">';
+        // Build product image HTML
+        $imageHtml = '';
         if (isset($item['product_url'])) {
-            $html .= '<a href="' . $item['product_url'] . '">' . $item['product_name'] . '</a>';
+            $imageUrl = $imageSrc ?: '/media/catalog/product/placeholder/default/image.jpg';
+            $imageHtml = '<a href="' . $item['product_url'] . '" class="product-item-photo" title="' . $item['product_name'] . '">
+                <span class="cart-product-image-container">
+                    <span class="cart-product-image-wrapper">
+                        <img class="cart-product-image-photo" src="' . $imageUrl . '" alt="' . $item['product_name'] . '" style="width: 120px;">
+                    </span>
+                </span>
+            </a>';
+        }
+        
+        // Build product name HTML
+        $nameHtml = '<strong class="product-item-name">';
+        if (isset($item['product_url'])) {
+            $nameHtml .= '<a href="' . $item['product_url'] . '">' . $item['product_name'] . '</a>';
         } else {
-            $html .= $item['product_name'];
+            $nameHtml .= $item['product_name'];
         }
-        $html .= '</strong>';
+        $nameHtml .= '</strong>';
         
-        // Product options
+        // Build product options HTML
+        $optionsHtml = '';
         if (isset($item['options']) && !empty($item['options'])) {
-            $html .= '<div class="product options">';
-            $html .= '<dl class="product options list">';
+            $optionsHtml = '<div class="product options">
+                <dl class="product options list">';
             foreach ($item['options'] as $option) {
-                $html .= '<dt class="label">' . $option['label'] . '</dt>';
-                $html .= '<dd class="values">';
-                $html .= '<span>' . $option['value'] . '</span>';
-                $html .= '</dd>';
+                $optionsHtml .= '<dt class="label">' . $option['label'] . '</dt>
+                    <dd class="values">
+                        <span>' . $option['value'] . '</span>
+                    </dd>';
             }
-            $html .= '</dl>';
-            $html .= '</div>';
+            $optionsHtml .= '</dl>
+            </div>';
         }
         
-        // Product pricing
-        $html .= '<div class="product-item-pricing">';
-        $html .= '<div class="price-container">';
-        $html .= '<span class="price-wrapper">';
-        $html .= '<span class="price">' . $item['product_price'] . '</span>';
-        $html .= '</span>';
-        $html .= '</div>';
-        
-        // Quantity
-        $html .= '<div class="details-qty qty">';
-        $html .= '<label class="label">Qty</label>';
-        $html .= '<input type="number" min="0" size="4" class="item-qty cart-item-qty" value="' . $item['qty'] . '">';
-        $html .= '</div>';
-        $html .= '</div>';
-        
-        // Product actions
-        $html .= '<div class="product actions">';
+        // Build actions HTML
+        $actionsHtml = '<div class="product actions">';
         if (isset($item['configure_url'])) {
-            $html .= '<div class="primary">';
-            $html .= '<a href="/checkout/cart/" class="action edit" title="Edit item">';
-            $html .= '<span>Edit</span>';
-            $html .= '</a>';
-            $html .= '</div>';
+            $actionsHtml .= '<div class="primary">
+                <a href="/checkout/cart/" class="action edit" title="Edit item">
+                    <span>Edit</span>
+                </a>
+            </div>';
         }
-        $html .= '<div class="secondary">';
-        $html .= '<a href="/checkout/cart/" class="action delete" data-cart-item="' . $item['item_id'] . '" title="Remove item">';
-        $html .= '<span>Remove</span>';
-        $html .= '</a>';
-        $html .= '</div>';
-        $html .= '</div>';
+        $actionsHtml .= '<div class="secondary">
+            <a href="/checkout/cart/" class="action delete" data-cart-item="' . $item['item_id'] . '" title="Remove item">
+                <span>Remove</span>
+            </a>
+        </div>
+        </div>';
         
-        $html .= '</div>'; // Close product-item-details
-        $html .= '</div>'; // Close product
-        $html .= '</li>';
+        // Build complete cart item HTML using heredoc
+        $html = <<<HTML
+        <li class="item product product-item" data-role="product-item">
+            <div class="product">
+                {$imageHtml}
+                <div class="product-item-details">
+                    {$nameHtml}
+                    {$optionsHtml}
+                    <div class="product-item-pricing">
+                        <div class="price-container">
+                            <span class="price-wrapper">
+                                <span class="price">{$item['product_price']}</span>
+                            </span>
+                        </div>
+                        <div class="details-qty qty">
+                            <label class="label">Qty</label>
+                            <input type="number" min="0" size="4" class="item-qty cart-item-qty" value="{$item['qty']}">
+                        </div>
+                    </div>
+                    {$actionsHtml}
+                </div>
+            </div>
+        </li>
+HTML;
         
         return $html;
     }
